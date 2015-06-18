e8da2002bd7ee5b9c6d789503aeb3fc1e68fdbc3
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2015-06-17 22:59:37 -0300

    RP - CNF

--- Regeneradores.cpp
+++ Regeneradores.cpp
@@ -1,6 +1,7 @@
 #include "Regeneradores.h"
 #include "Def.h"
 #include "Main_Auxiliar.h"
+#include "RWA.h"
 
 Regeneradores::Regeneradores() {};
 
@@ -18,6 +19,39 @@ void Regeneradores::RP_NDF(int NumTotalRegeneradores, int NumRegeneradoresPorNo)
 		vector<int> NosMaximos;
 		for (int j = 0; j < GrauNo.size(); j++)
 			if (GrauNo.at(j) == MaiorGrau) NosMaximos.push_back(j);
+
+		int NoEscolhido = ceil( General::uniforme(0, NosMaximos.size()) );
+		MAux::Rede.at(NoEscolhido).set_TipoNo( Node::Translucido );
+	}
+}
+
+void Regeneradores::RP_CNF(int NumTotalRegeneradores, int NumRegeneradoresPorNo) {
+	assert (NumTotalRegeneradores % NumRegeneradoresPorNo == 0);
+	int NumNoTransparentes = NumTotalRegeneradores/NumRegeneradoresPorNo;
+	assert(NumNoTransparentes <= Def::Nnodes);
+
+	vector <int> CentralidadeNo(Def::Nnodes);
+	for (int i = 0; i < CentralidadeNo.size(); i++) CentralidadeNo.at(i) = 0;
+
+	MAux MainAux;
+	RWA::DijkstraSP(&MainAux);
+
+	for (int orN = 0; orN < Def::getNnodes(); orN++) {
+		for (int deN = 0; deN < Def::getNnodes(); deN++) {
+			if (orN == deN) continue;
+			for (int No = 0; No <= MainAux.AllRoutes[orN*Def::Nnodes + deN].at(0)->getNhops(); No++)
+				CentralidadeNo.at(MainAux.AllRoutes[orN*Def::Nnodes + deN].at(0)->getNode(No))++;
+		}
+	}
+
+	for (int i = 0; i < NumNoTransparentes; i++) {
+		int MaiorGrau = CentralidadeNo.at(0);
+		for (int j = 0; j < CentralidadeNo.size(); j++)
+			if (MaiorGrau < CentralidadeNo.at(j)) MaiorGrau = CentralidadeNo.at(j);
+
+		vector<int> NosMaximos;
+		for (int j = 0; j < CentralidadeNo.size(); j++)
+			if (CentralidadeNo.at(j) == MaiorGrau) NosMaximos.push_back(j);
 
 		int NoEscolhido = ceil( General::uniforme(0, NosMaximos.size()) );
 		MAux::Rede.at(NoEscolhido).set_TipoNo( Node::Translucido );
--- Regeneradores.h
+++ Regeneradores.h
@@ -6,7 +6,7 @@ class Regeneradores {
 		Regeneradores();
 
 		static void RP_NDF(int NumTotalRegeneradores, int NumRegeneradoresPorNo); //Nodal Degree First
-		static void RP_CDF(int NumTotalRegeneradores, int NumRegeneradoresPorNo); //Centered Node First
+		static void RP_CNF(int NumTotalRegeneradores, int NumRegeneradoresPorNo); //Centered Node First
 		static void RP_TLP(int NumTotalRegeneradores, int NumRegeneradoresPorNo); //Traffic Load Prediction Based
 		static void RP_SQP(int NumTotalRegeneradores, int NumRegeneradoresPorNo); //Signal Quality Prediction Based
 };
